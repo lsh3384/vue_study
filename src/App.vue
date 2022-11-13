a79f673bf2d7c28fe56bbcadf747c60c1528c386
lee <lsh3384@gmail.com>
2022-11-13 22:57:59 +0900

    adding todo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="This is Vue Study"/>
+  <TodoApp />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoApp from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoApp
   }
 }
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+    <input v-model="new_todo_content" />
+    <button v-on:click="add()" >추가</button>
+    
+    <div class="todos">
+        <div v-for="(todo) in todos" :key="todo.id">
+                {{todo.completed}} {{todo.content }}
+                <button>삭제</button>
+        </div>
+    </div>
+    <button v-on:click="test_print()">테스트 출력</button>
+</template>
+
+<script>
+export default {
+    name: 'TodoApp',
+    data() {
+        return {
+            todos: [{id: 0, completed: 'false', content: "독서"}],
+            new_todo_content: "",
+        }
+    },
+    methods: {
+        add() {
+            this.todos.push({id: this.todos.length, completed: 'false', content: this.new_todo_content});
+            this.new_todos = "";
+        },
+        test_print() {
+            console.log(this.new_todo_content);
+        }
+    }
+}
+</script>
