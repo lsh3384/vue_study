53fa1f8b2bf7b1295a607292d43f45c6e731b234
lee <lsh3384@gmail.com>
2022-11-14 23:45:36 +0900

    added delete_todo method

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,11 +1,11 @@
 <template>
     <input v-model="new_todo_content" />
-    <button v-on:click="add()" >추가</button>
+    <button v-on:click="add_todo()" >추가</button>
     
     <div class="todos">
         <div v-for="(todo) in todos" :key="todo.id">
                 {{todo.completed}} {{todo.content }}
-                <button>삭제</button>
+                <button v-on:click="delete_todo(todo.id)" >삭제</button>
         </div>
     </div>
     <button v-on:click="test_print()">테스트 출력</button>
@@ -21,9 +21,19 @@ export default {
         }
     },
     methods: {
-        add() {
+        add_todo() {
             this.todos.push({id: this.todos.length, completed: 'false', content: this.new_todo_content});
             this.new_todos = "";
+        },
+        delete_todo(id) {
+            console.log('delete!');
+            console.log(id);
+            this.todos.map((ele, index) => {
+                console.log(ele.id);
+                if(ele.id === id) {
+                    this.todos.splice(index, 1)
+                }
+            })
         },
         test_print() {
             console.log(this.new_todo_content);
